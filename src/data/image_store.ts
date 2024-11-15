151f5a9cf15c757358b3b9f31e8d2728171d2bc8
Adam Dubey <adubey0311@gmail.com>
2024-11-15 09:35:36 -0500

    [AD][111524] Create data for testing

---
+++ src/data/image_store.ts
@@ -0,0 +1,17 @@
+export const ALL_IMAGES = [
+    {
+        id: 3,
+        url: 'https://bar.com/img1',
+        author: 'Adam Ops'
+    },
+    {
+        id: 2,
+        url: 'https://baz.com/img2',
+        author: 'Program Jim'
+    },
+    {
+        id: 1,
+        url: 'https://foo.com/img1',
+        author: 'Dev Simpson'
+    }
+]
