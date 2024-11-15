c065c8dedf7b4499d2db0dcfc0b5b48330937163
Adam Dubey <adubey0311@gmail.com>
2024-11-15 18:51:06 -0500

    [AD][111524] Update index.ts with new routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { Router } from 'itty-router'
 import getImages from './handlers/get_images'
+import getSingleImage from './handlers/get_single_image';
+import createImage from './handlers/create_image';
 
 const router = Router()
 
 router.get('/images', getImages)
+	.get('/images/:id', getSingleImage)
+	.post('/images', createImage)
 	.get('*', () => new Response('Not found', { status: 404 }));
+	
 export interface Env {
 // MY_KV_NAMESPACE: KVNamespace;
 }
